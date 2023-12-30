654e13eb953f0eb34d42b9700e0e2c8703d91f37
Mo_laptop <moalobaidly@gmail.com>
2023-12-30 22:00:23 +0000

    first change?

---
+++ tic tac toe.py
@@ -0,0 +1,13 @@
+board = [0,0,0,0,0,0,0,0,0]
+def printboard():
+    print(f"  |   |   ")
+    print(f"{board[0]} | {board[1]} | {board[2]} ")
+    print("----------")
+    print(f"{board[3]} | {board[4]} | {board[5]} ")
+    print("----------")
+    print(f"{board[6]} | {board[7]} | {board[8]} ")
+    print(f"  |   |   ")
+
+
+
+printboard()
